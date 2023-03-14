e1f7a0dd7f22af2405c39f237d7c373e62d4fd39
David Barcia <david.barcia@tsoftlatam.com>
2023-03-14 15:44:31 -0300

    modificacion de token

--- token.sh
+++ token.sh
@@ -10,16 +10,15 @@ echo $token>token.txt
 export creacion=$(curl --location "https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart" \
 --header "Authorization: Bearer $token" \
 --header 'Content-Type: multipart/form-data' \
---form info=@"/C:/Users/David.Barcia/Desktop/Documentos/info.json" \
---form results=@"/C:/Users/David.Barcia/Desktop/Documentos/Cucumberprueba.json")
+--form info=@"info.json" \
+--form results=@"Cucumberprueba.json")
 
 echo $creacion>crear.txt
 
 export key=$(grep "IXR" crear.txt | cut -d '"' -f 8)
 
-echo "Imprimiendo key : " $key
 export url="https://integracionx-ray.atlassian.net/rest/api/3/issue/${key}/attachments"
 curl --location -g $url \
 --header 'X-Atlassian-Token: no-check' \
 --header 'Authorization: Basic ZGF2aWQuYmFyY2lhQHRzb2Z0bGF0YW0uY29tOkFUQVRUM3hGZkdGMHd1R0poN0RBM2lULUtPbm41clM2LUJzTmFFWmRzUlA4R0dfNFFVdnM4UFZ5Z000VmVLbDc1dko4WWtJdUh2RXB4YTdLRDJxSEdoZG1kU203LWpDb0RkWElRRWtDb1JjVm15OTRBQnVHcnRfSFB3elE4ZXFLak8zY3lrUjU0bVVza3NiZS03OFZkdHpZZFZma2lNWVdHLVRQNWZ1NUozVTdIakZqX3YwMm9fZz1CQTI4Rjk4Ng==' \
---form file=@"/C:/Users/David.Barcia/Downloads/reporteQA.pdf"
+--form file=@"reporteQA.pdf"
